801dfc9c6d8a77e2d541c875a91e12ee850747b7
Pariwat <wat_promjai@hotmail.com>
2014-08-01 22:05:16 +0700

    member
    member

---
+++ RatreeSamosorn/RatreeSamosorn/ViewController/Update/Detail/PFDetailViewController.m
@@ -0,0 +1,86 @@
+//
+//  PFDetailViewController.m
+//  ราตรีสโมสร
+//
+//  Created by Pariwat on 8/1/14.
+//  Copyright (c) 2014 platwofusion. All rights reserved.
+//
+
+#import "PFDetailViewController.h"
+
+@interface PFDetailViewController ()
+
+@end
+
+@implementation PFDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+        [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.tableView.tableHeaderView = self.headerView;
+    UIView *fv = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 10)];
+    self.tableView.tableFooterView = fv;
+    
+    if (IS_WIDESCREEN) {
+        self.textCommentView.frame = CGRectMake(0, 464+60, 320, 356);
+    } else {
+        self.textCommentView.frame = CGRectMake(0, 440, 320, 356);
+    }
+    
+    [self.view addSubview:self.textCommentView];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSUInteger)supportedInterfaceOrientations{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 85;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    PFDetailCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PFDetailCell"];
+    if(cell == nil) {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"PFDetailCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    return cell;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    if ([self.navigationController.viewControllers indexOfObject:self] == NSNotFound) {
+        // 'Back' button was pressed.  We know this is true because self is no longer
+        // in the navigation stack.
+        if([self.delegate respondsToSelector:@selector(PFDetailViewControllerBack)]){
+            [self.delegate PFDetailViewControllerBack];
+        }
+    }
+}
+
+@end
